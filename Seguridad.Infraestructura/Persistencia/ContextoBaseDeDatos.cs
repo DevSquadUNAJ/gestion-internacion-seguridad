de7abf18b079854c7bcced00e350e55976664780
AgustinMartinez03 <agustincarpmartinez@gmail.com>
2026-06-03 10:10:15 -0300

    feat(infraestructura): crear ContextoBaseDeDatos

---
+++ Seguridad.Infraestructura/Persistencia/ContextoBaseDeDatos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Seguridad.Dominio.Entidades;
+using System.Reflection;
+
+
+namespace Seguridad.Infraestructura.Persistencia
+{
+    public class ContextoBaseDeDatos : DbContext
+    {
+        public ContextoBaseDeDatos(DbContextOptions<ContextoBaseDeDatos> opciones) : base(opciones)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
