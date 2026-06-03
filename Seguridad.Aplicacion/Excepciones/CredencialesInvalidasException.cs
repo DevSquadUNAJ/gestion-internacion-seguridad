1a446b23f2354c823a19cd02a6ac46aa8afdc744
AgustinMartinez03 <agustincarpmartinez@gmail.com>
2026-06-03 11:17:39 -0300

    feat(aplicacion): implementar DTOs, excepciones, mapeadores e IniciarSesionCasoDeUso con validacion BCrypt

---
+++ Seguridad.Aplicacion/Excepciones/CredencialesInvalidasException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Seguridad.Aplicacion.Excepciones
+{
+    public class CredencialesInvalidasException : Exception
+    {
+        public CredencialesInvalidasException() : base("El usuario o la contraseña son incorrectos.")
+        {
+        }
+    }
+}
---
+++ Seguridad.Aplicacion/Interfaces/IMapeadores/IIniciarSesionMapeador.cs
@@ -0,0 +1,10 @@
+﻿using Seguridad.Aplicacion.DTOs.Respuestas;
+using Seguridad.Dominio.Entidades;
+
+namespace Seguridad.Aplicacion.Interfaces.IMapeadores
+{
+    public interface IIniciarSesionMapeador
+    {
+        IniciarSesionRespuesta MapearARespuesta(Usuario usuario, string token);
+    }
+}
